acc8ffee23db6131c2f754c2890b380a86347d58
hrishikesh khot <hrishikeshkhot33@gmail.com>
2024-09-26 05:36:04 -0700

    controller added

--- src/main/java/com/example/demo/controllers/Controller.java
+++ src/main/java/com/example/demo/controllers/Controller.java
@@ -1,13 +1,24 @@
 package com.example.demo.controllers;
 
+import com.example.demo.services.userService;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 
+    @Autowired
+    private userService userservice;
+
+    public Controller(userService service) {
+        this.userservice =  service;
+    }
+
     @GetMapping("/")
     public String hello(){
-        return "hello world";
+
+        return userservice.getHello();
     }
 }
---
+++ src/main/java/com/example/demo/services/userService.java
@@ -0,0 +1,11 @@
+package com.example.demo.services;
+
+
+@org.springframework.stereotype.Service
+public class userService {
+
+
+    public String getHello(){
+        return "hello world";
+    }
+}
